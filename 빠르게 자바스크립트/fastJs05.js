8ab26806327c9dc6c76f9749925961611f55e6b3
maria5845 <younglee921215@gmail.com>
2020-12-02 23:00:36 +0900

    프로토타입 부분 추가

--- 빠르게 자바스크립트/fastJs05.js
+++ 빠르게 자바스크립트/fastJs05.js
@@ -17,16 +17,28 @@ var a = answer();
 function Car(a,b,c){
     this.name =a; // 공용변수
     this.color =b;
-    var move = c; // private 변수 
-    // name 과 color는 맴버라고 한다.
+
+}
+Car.prototype.move = function () {
+    console.log(this.name + '차이고'+ this.color+"색입니다.");
 }
 
 var a = new Car("현대","노란","전진");
+a.move();
 console.log(a.name);
 console.log(a.color);
-console.log(a.move);
-
 
 var b = new Car("기아","파란");
+b.move();
 console.log(b.name);
 console.log(b.color);
+
+var a = [1,2,3,10]
+Array.prototype.print = function(){
+    for(var i=0; i<this.length; i++){
+        console.log(i);        
+    }
+}
+a.print();
+a.length 
+
